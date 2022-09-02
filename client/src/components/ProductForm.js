90934c73d8c54b0dd0232414c199cb0455df688d
JacobG321 <jacobgruber1998@gmail.com>
2022-09-02 18:20:34 -0400

    "fixed client"

---
+++ client/src/components/ProductForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+const ProductForm = () => {
+    const [title, setTitle] = useState(""); 
+    const [price, setPrice] = useState("");
+    const [description, setDescription] = useState("");
+
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        // make sure you change axios post
+        axios.post('http://localhost:8000/api/product', { 
+            title,
+            price,
+            description
+        })
+            .then(res=>{
+                console.log(res);
+                console.log(res.data);
+            })
+            .catch(err=>console.log(err))
+    }
+    
+    return (
+        <form onSubmit={onSubmitHandler}>
+
+                <label htmlFor='title'>Title</label>
+                <input type="text" name='title' onChange = {(e)=>setTitle(e.target.value)}/>
+
+                <label htmlFor='price'>Price</label>
+                <input type="text" name="price" onChange = {(e)=>setPrice(e.target.value)}/>
+
+                <label htmlFor='description'>Description</label>
+                <input type="text" name="description" onChange = {(e)=>setDescription(e.target.value)}/>
+
+                <input type="submit"/>
+        </form>
+    )
+}
+export default ProductForm;
